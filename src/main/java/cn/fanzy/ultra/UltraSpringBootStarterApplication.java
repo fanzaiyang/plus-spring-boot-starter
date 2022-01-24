8adc165b51ca11087cbd8c245c0e6ed2eae9d00d
fanzaiyang <zaiyangnihao@163.com>
2022-01-24 15:14:32 +0800

    初始化项目

---
+++ src/main/java/cn/fanzy/ultra/UltraSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package cn.fanzy.ultra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltraSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltraSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/fanzy/ultra/UltraSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fanzy.ultra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltraSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
